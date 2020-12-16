2e782be3d83c582a7ac180a5a3dc7261591c1ec9
Filip Bartosiak <filipbart1@gmail.com>
2020-12-16 12:57:35 +0100

    Http request for orders and improving the code

--- lib/providers/product.dart
+++ lib/providers/product.dart
@@ -1,6 +1,9 @@
-import 'package:flutter/foundation.dart';
+import 'dart:convert';
 
-class Product with ChangeNotifier{
+import 'package:flutter/foundation.dart';
+import 'package:http/http.dart' as http;
+
+class Product with ChangeNotifier {
   final String id;
   final String title;
   final String description;
@@ -17,8 +20,29 @@ class Product with ChangeNotifier{
     this.isFavorite = false,
   });
 
-  void toggleFavroiteStatus(){
+  void _setFavValue(bool newValue) {
+    isFavorite = newValue;
+    notifyListeners();
+  }
+
+  Future<void> toggleFavroiteStatus() async {
+    final oldStatus = isFavorite;
     isFavorite = !isFavorite;
     notifyListeners();
+    final url =
+        'https://shop-app-flutter-acda0-default-rtdb.europe-west1.firebasedatabase.app/products/$id.json';
+    try {
+      final response = await http.patch(
+        url,
+        body: json.encode({
+          'isFavorite': isFavorite,
+        }),
+      );
+      if (response.statusCode >= 400) {
+        _setFavValue(oldStatus);
+      }
+    } catch (error) {
+      _setFavValue(oldStatus);
+    }
   }
 }
